cfbdb0ebe7717c8a40664b4040b252b27aa1bf9d
dark-flare <dark@neckbeards.co.uk>
2013-04-12 20:22:26 +0200

    Firefox bug fixed

--- QuackRemove.js
+++ QuackRemove.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name           Quack Remover
-// @namespace      Failheap
-// @description    Hides any post QuackBot makes, and any post that quotes a post QuackBot makes
-// @include        http://failheap-challenge.com/*
-// @grant              none
+// @name Quack Remover
+// @namespace Failheap
+// @description Hides any post QuackBot makes, and any post that quotes a post QuackBot makes
+// @include http://failheap-challenge.com/*
+// @grant none
 // ==/UserScript==
 //
 (function (){
@@ -13,8 +13,8 @@ var allElements = document.evaluate("//div[@class='userinfo']",
                 document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
 for (var i = allElements.snapshotLength -1; i >= 0; i--){
     thisElement = allElements.snapshotItem(i);
-    if((thisElement.innerHTML.indexOf("QuackBot")!=-1) || (thisElement.parentNode.innerHTML.indexOf("Originally posted by QuackBot")!=-1)){
-				thisElement.parentNode.style.display = 'none';
+    if((thisElement.innerHTML.indexOf("QuackBot")!=-1) || (thisElement.parentNode.innerHTML.indexOf("Originally Posted by QuackBot")!=-1) || (thisElement.parentNode.innerHTML.indexOf("Originally Posted by <strong>QuackBot</strong>")!=-1)){
+thisElement.parentNode.style.display = 'none';
                 }
         }
 }());
